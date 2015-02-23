95bb730f015aa26a0427a4b5d93f60712f210872
MetalMercante <sandorcaetano@yahoo.com.br>
2015-02-22 22:33:08 -0300

    consegui!

--- graficos.R
+++ graficos.R
@@ -24,31 +24,10 @@ for (i in 1:length(notas)){
         acumulado[i] <- total
 }
 
-dados <- data.frame(ac=acumulado,musicas=musicas, id=1:13)
+dados <- data.frame(ac=acumulado,musicas=musicas)
 dados$musicas <- factor(dados$musicas, levels = dados$musicas, ordered = TRUE)
-dados$acumulado <- factor(dados$acumulado, levels = dados$acumulado, ordered = TRUE)
-dados$id <- factor(dados$id, levels = dados$id, ordered = TRUE)
-
-plot(acumulado)
-plot(notas)
 
 
-qplot(data=dados,x=musicas,y=ac)
 
-ggplot(dados,x=musicas, y=ac)) + geom_line(group=dados$ac)
-ggplot(dados,aes(x=musicas,y=ac))+geom_line()
+ggplot(data=dados,aes(x=musicas, y=acumulado, group=1)) + geom_line()
 
-ggplot(dados)+geom_line(aes(y=dados$acumulado,x=dados$musicas))
-
-p <- ggplot(dados, aes(x=acumulado, y=id))
-p + geom_line()
-
-
-df <- read.csv("F://Drive//ML-Conf//ML-Conferencia//Pasta2.csv",sep=";")
-
-pd <- position_dodge(.1)
-ggplot(df, aes(x=iter, y=perf, colour=name)) + 
-        geom_errorbar(aes(ymin=perf-se, ymax=perf+se), width=.1, position=pd) +
-        geom_line(position=pd,aes(group=name)) +
-        geom_point(position=pd)+
-        ylim(0, 20)
